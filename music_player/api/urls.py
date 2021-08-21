3095dcb3e46a689e183b4187cba45d05d3a352ee
Aviral Vaid <avi.aviralvaid@gmail.com>
2021-08-20 18:09:03 -0700

    Started working on the backend api

--- music_player/api/urls.py
+++ music_player/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from .views import home
-
+from .views import UserCommentsApiViewAll
+from .views import UserCommentsApiViewLatest
+from .views import UserCommentsApiViewPost
 urlpatterns = [
-    path('', home)
+    path('api/all', UserCommentsApiViewAll.as_view()), 
+    path('api/latest', UserCommentsApiViewLatest.as_view()), 
+    path('api/post', UserCommentsApiViewPost.as_view()), 
 ]
